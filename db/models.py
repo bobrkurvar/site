49971bb2a3dff06d5028f163fe14c8d3dc78941a
bobrkurvar <andrey.bogdanov2005@mail.ru>
2025-11-14 21:55:56 +0300

    добавил модель для размерности плиток и модель р

--- db/models.py
+++ db/models.py
@@ -1,5 +1,6 @@
+from sqlalchemy import ForeignKeyConstraint
 from sqlalchemy.ext.asyncio import AsyncAttrs
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from core import config
 
 
@@ -11,12 +12,35 @@ class Catalog(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(unique=True)
     image_path: Mapped[str] = mapped_column(default=config.image_path)
-    price: Mapped[float]
+    size_height: Mapped[float]
+    size_width: Mapped[float]
+
+    size: Mapped['SizeTile'] = relationship("SizeTile", back_populates="tiles")
+
+    __table_args__ = (
+        ForeignKeyConstraint(
+            ['size_height', 'size_width'],
+            ['tile_size.height', 'tile_size.width']
+        ),
+    )
 
     def model_dump(self):
         return {
             "id": self.id,
             "name": self.name,
-            "price": self.price,
+            "size_height": self.size_height,
+            "size_width": self.size_width,
             "image_path": self.image_path
+        }
+
+class SizeTile(Base):
+    __tablename__ = 'tile_size'
+    height: Mapped[float] = mapped_column(primary_key=True)
+    width: Mapped[float] = mapped_column(primary_key=True)
+    tiles: Mapped[list['Catalog']] = relationship("Catalog", back_populates='size')
+
+    def model_dump(self):
+        return {
+            "height": self.height,
+            "width": self.width
         }
---
+++ domain/tile_size.py
@@ -0,0 +1,6 @@
+
+class TileSize:
+
+    def __init__(self, height: int, width: int):
+        self.height = height
+        self.width = width
