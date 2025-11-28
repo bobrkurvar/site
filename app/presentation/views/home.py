eeb889469982133d372f65d5956998d337901d04
Andy <adnrey.bogdanov2005@mail.ru>
2025-11-28 09:40:50 +0300

    списки фото в деталях и динамический слайдер по количеству слайдов

--- app/presentation/views/home.py
+++ app/presentation/views/home.py
@@ -4,8 +4,8 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, Request
 from fastapi.templating import Jinja2Templates
 
-from domain.tile import Producer
 from repo import Crud, get_db_manager
+from pathlib import Path
 
 router = APIRouter()
 templates = Jinja2Templates("templates")
@@ -15,10 +15,14 @@ log = logging.getLogger(__name__)
 
 
 @router.get("/")
-async def get_main_page(request: Request, manager: dbManagerDep):
+async def get_main_page(request: Request):
+    slides_dir = Path.cwd() / "static" / "images" / "slides"
+
+    slide_images = [f"/static/images/slides/{img.name}" for img in slides_dir.iterdir() if img.is_file()]
     return templates.TemplateResponse(
         "home.html",
         {
             "request": request,
+            "slide_images": slide_images
         },
     )
