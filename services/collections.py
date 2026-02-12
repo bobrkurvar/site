43e7bcc3908c31df121ea0042d4b1f6f8f273fb0
Andy <adnrey.bogdanov2005@mail.ru>
2026-02-12 20:11:14 +0300

    поправил создание коллекций

--- services/collections.py
+++ services/collections.py
@@ -19,29 +19,30 @@ async def add_collection(
 
     async with uow_class(manager._session_factory) as uow:
         image_path = file_manager.base_collection_path(name)
-        collection_record = await manager.create(
-            Collections,
-            name=name,
-            image_path=str(image_path),
-            session=uow.session,
-        )
+        collection_record = await manager.read(Collections, name=name, session=uow.session)
+        if not collection_record:
+            collection_record = await manager.create(
+                Collections,
+                name=name,
+                image_path=str(image_path),
+                session=uow.session,
+            )
+            try:
+                async with file_manager.session() as files:
+                    await files.save(image_path, image)
+                    miniatures = await generate_images(image)
+                    for layer, miniature in miniatures.items():
+                        await files.save_by_layer(image_path, miniature, layer)
+            except TypeError:
+                log.debug(
+                    "generate_image_variant_callback  или save_files не получили нужную функцию"
+                )
+                raise
+            except FileExistsError:
+                log.debug("путь %s уже занять", image_path)
+                raise
         await manager.create(CollectionCategory, collection_name=name, category_name=category_name, session=uow.session)
         await manager.create(Slug, name=name, slug=slugify(name))
-        try:
-            async with file_manager.session() as files:
-                await files.save(image_path, image)
-                miniatures = await generate_images(image)
-                for layer, miniature in miniatures.items():
-                    await files.save_by_layer(image_path, miniature, layer)
-        except TypeError:
-            log.debug(
-                "generate_image_variant_callback  или save_files не получили нужную функцию"
-            )
-            raise
-        except FileExistsError:
-            log.debug("путь %s уже занять", image_path)
-            raise
-
         return collection_record
 
 
