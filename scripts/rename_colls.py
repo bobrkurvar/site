963c771163ac13bba0d2ecad0c674743920c4e51
bobrkurvar <andrey.bogdanov2005@mail.ru>
2026-02-12 12:28:35 +0300

    добавил новую таблицу для коллекций-категорий

--- scripts/rename_colls.py
+++ scripts/rename_colls.py
@@ -3,7 +3,7 @@ import logging
 from pathlib import Path
 
 import core.logger
-from adapters.repo import get_db_manager
+from adapters.crud import get_db_manager
 from domain.tile import Collections
 
 log = logging.getLogger(__name__)
@@ -11,37 +11,37 @@ log = logging.getLogger(__name__)
 
 async def new_images_paths_for_collections():
     manager = get_db_manager()
+    manager.connect()
     collections = await manager.read(Collections)
     for collection in collections:
         old_path = Path(collection["image_path"])
         path_parts = list(Path(old_path).parts)
-        if path_parts[4].isdigit():
-            path_parts[4] = f"{collection['name']}-{collection['category_name']}"
-            new_path = Path(*path_parts)
-            old_paths = [
-                old_path,
-                Path("static/images/collections/catalog") / old_path.name,
-            ]
-            new_paths = [
-                new_path,
-                Path("static/images/collections/catalog") / new_path.name,
-            ]
-            for old, new in zip(old_paths, new_paths):
-                try:
-                    old.rename(new)
-                except FileExistsError:
-                    print(f"Папка {new} уже существует. Пропускаем.")
-                except FileNotFoundError:
-                    print(f"Папкане {old} найдена.")
-                await manager.update(
-                    Collections,
-                    {
-                        "name": collection["name"],
-                        "category_name": collection["category_name"],
-                    },
-                    image_path=str(new_path),
-                )
-
+        path_parts[-1] = f"{collection['name']}"
+        new_path = Path(*path_parts)
+        log.debug("old path: %s, new path: %s", old_path, new_path)
+        old_paths = [
+            old_path,
+            Path("static/images/collections/catalog") / old_path.name,
+        ]
+        new_paths = [
+            new_path,
+            Path("static/images/collections/catalog") / new_path.name,
+        ]
+        for old, new in zip(old_paths, new_paths):
+            try:
+                old.rename(new)
+            except FileExistsError:
+                log.debug("Файл %s уже существует. Пропускаем.", new)
+            except FileNotFoundError:
+                log.debug("Файл %s не найден.", old)
+            await manager.update(
+                Collections,
+                {
+                    "name": collection["name"],
+                    "category_name": collection["category_name"],
+                },
+                image_path=str(new_path),
+            )
 
 async def main():
     await new_images_paths_for_collections()
